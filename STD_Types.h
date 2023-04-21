d7bda7e17428fb477101fc77bf30e76d9d92e0d4
ZiadElmimy <ziadelmimy@gmail.com>
2023-04-21 02:09:01 +0200

    Standard types used in the drivers

---
+++ STD_Types.h
@@ -0,0 +1,23 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+typedef unsigned long long u128;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long s64;
+typedef signed long long s128;
+
+typedef enum{
+	    Flase = 0,
+		True
+	}Bool;
+
+
+
+#endif /* STD_TYPES_H_ */
